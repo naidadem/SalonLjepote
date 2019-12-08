6e8e621ca0fbc5fe93a893a10a23b13987da8bf0
Naida Demirovic <naida.demirovic@tetrapak.com>
2019-12-08 17:02:22 +0100

    Added Web Service

--- eHairdresserSalon-master/WebApplication2/Scripts/custom.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(() => {
-    console.log('ready');
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-});
-    
-});
---
+++ eHairdresserSalon-master/WebSalonLjepote/Global.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSalonLjepote
+{
+    public class Global
+    {
+
+        //public static List<VrsteProizvoda> vrste = new List<VrsteProizvoda>();
+
+        //public static bool pregledaneObavijesti = false;
+
+        public static string APIAddress = "http://ndemirovic.app.fit.ba/";
+
+
+        #region API Routes
+
+        public const string dodatniTretmaniRoute = "api/DodatniTretmani";
+        public const string dodatniTretmaniOcjeneRoute = "api/DodatniTretmaniOcjene";
+        public const string frizeriRoute = "api/Frizeri";
+
+        //public const string proizvodiRoute = "api/proizvodi";
+        //public const string ulogeRoute = "api/uloge";
+        //public const string korisniciUlogeRoute = "api/korisniciUloge";
+        //public const string upitiRoute = "api/upiti";
+        //public const string odgovoriRoute = "api/Odgovori";
+        //public const string ocjeneRoute = "api/ocjene";
+        //public const string notifikacijeRoute = "api/notifikacije";
+        //public const string autaRoute = "api/auta";
+        //public const string akcijaProizvodiRoute = "api/akcijaProizvodi";
+
+        #endregion
+    }
+}
