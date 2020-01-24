23074d9b5238019e6efdb0dc16c14d45116bbade
Naida Demirovic <naida.demirovic@tetrapak.com>
2020-01-25 00:09:19 +0100

    Ready to include Web
    Just commit to add to web

--- eHairdresserSalon-master/WebBeautySalon/Controllers/UslugeController.cs
+++ eHairdresserSalon-master/WebBeautySalon/Controllers/UslugeController.cs
@@ -13,8 +13,11 @@ namespace WebBeautySalon.Controllers
     public class UslugeController : Controller
     {
         private WebAPIHelper uslugeService = new WebAPIHelper(Global.APIAddress, "api/Usluge");
+        private WebAPIHelper vrsteUslugaService = new WebAPIHelper(Global.APIAddress, "api/VrsteUsluga");
 
         List<Usluge_Result> usluge = new List<Usluge_Result>();
+        List<VrsteUsluga> vrste = new List<VrsteUsluga>();
+
 
         public IActionResult Index(int vrstaId = 1)
         {
@@ -24,15 +27,111 @@ namespace WebBeautySalon.Controllers
                 usluge.Clear();
                 usluge = responseUsluge.Content.ReadAsAsync<List<Usluge_Result>>().Result;
             }
+            
+            HttpResponseMessage responseVrste = vrsteUslugaService.GetResponse();
+            if (responseVrste.IsSuccessStatusCode)
+            {
+                vrste = responseVrste.Content.ReadAsAsync<List<VrsteUsluga>>().Result;
+            }
 
-            ViewData["vrste"] = Global.vrste;
+            ViewData["vrste"] = vrste;
             ViewData["odabranaVrsta"] = vrstaId;
             return View(usluge);
         }
 
-        public IActionResult Edit()
+        //public IActionResult Dodaj()
+        //{
+        //    Usluge u = new Usluge();
+        //    ViewData["kljuc1"] = u;
+        //    return View("Edit");
+        //}
+
+        [HttpGet]
+        public IActionResult Edit(int id)
         {
-            return View();
+            int uID = Convert.ToInt32(id);
+            Usluge mu = usluge.Where(x => x.UslugaID == uID).ToList();
+            return View(u);
         }
+
+        //[HttpPost]
+        //public IActionResult Edit(Usluge u)
+        //{
+        //    var uid = u.UslugaID;
+        //    var nu=u.NazivUsluge;
+        //    var tm = u.TrajanjeMinute;
+        //    var cu = u.CijenaUsluge;
+
+        //    //if (vrstaUslugeComboBox.SelectedIndex != 0)
+        //    //    u.VrstaUslugeID = Convert.ToInt32(vrstaUslugeComboBox.SelectedValue);
+        //    var vuid = u.VrstaUslugeID;
+
+        //    return RedirectToAction("Index");
+        //}
+        //[HttpPost]
+        //public IActionResult Doda()
+        //{
+        //    Usluge u = new Usluge();
+        //    u.NazivUsluge = NazivUslugeInput.Text;
+        //    u.TrajanjeMinute = Convert.ToInt32(TrajanjeUslugeInput.Text);
+        //    u.CijenaUsluge = Convert.ToDecimal(CijenaUslugeInput.Text);
+
+        //    if (vrstaUslugeComboBox.SelectedIndex != 0)
+        //        u.VrstaUslugeID = Convert.ToInt32(vrstaUslugeComboBox.SelectedValue);
+
+        //    HttpResponseMessage response = uslugeService.PostResponse(u);
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        MessageBox.Show("Usluga je uspjesno dodana.");
+        //        DialogResult = DialogResult.OK;
+        //        Close();
+        //    }
+        //    else
+        //    {
+        //        string msg = response.ReasonPhrase;
+        //        if (!String.IsNullOrEmpty(Messages.ResourceManager.GetString(response.ReasonPhrase)))
+        //            msg = Messages.ResourceManager.GetString(response.ReasonPhrase);
+
+        //        MessageBox.Show("Error code: " + response.StatusCode + " Message: " + msg);
+
+        //    }
+
+        //    vm.VrsteProizvoda = GetVrsteProizvodaAll();
+        //    vm.Proizvodi = GetProizvodiAll();
+        //    vm.Auta = GetAutaAll();
+        //    vm.VrstaProizvodaID = vm.VrstaProizvodaID;
+        //    ViewData["vrste"] = Global.vrste;
+        //    return View(vm);
+        //}
+
+
+        //public IActionResult Snimi(int uslugaID, string NazivUsluge, int TrajanjeMinute, decimal CijenaUsluge, int VrstaUslugeID)
+        //{
+        //    Usluge u;
+        //    if (uslugaID != 0)
+        //    {
+        //        u = _db._studenti.Find(studentid);
+        //    }
+        //    else
+        //    {
+        //        s = new Student();
+        //        _db._studenti.Add(s);
+        //    }
+
+        //    s.Ime = ime;
+        //    s.OpstinaId = opstinaId;
+        //    s.Prezime = prezime;
+        //    _db.SaveChanges();
+
+        //    return RedirectToAction("Index");
+        //}
+
+        //public IActionResult Obrisi(int id)
+        //{
+        //    Student p1 = _db._studenti.Where(x => x.Id == id).FirstOrDefault();
+        //    _db._studenti.Remove(p1);
+        //    _db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
     }
 }
--- eHairdresserSalon-master/WebBeautySalon/wwwroot/js/site.js
+++ eHairdresserSalon-master/WebBeautySalon/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+jQuery(document).ready(function () {
+    console.log('test0');
+    $.urlParam = function (name) {
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        return results[1] || 1;
+    }
+    var vrstaID = $.urlParam('vrstaId');
+    if (vrstaID > 0) {
+        $('select').find('option[value="' + vrstaID + '"]').attr("selected", "selected")
+    } 
+
+})
